f6e9c70c0542c81913508f23f167d582e4f3a6c0
lee <iop2480@naver.com>
2020-03-27 00:09:59 +0900

    heart icon 변경

--- src/components/book-detail.vue
+++ src/components/book-detail.vue
@@ -41,10 +41,8 @@
                   type="is-text"
                   style="padding: 0; color: #fb366e;"
                 > -->
-                  <!-- <i @click="toggleFavorite" v-if="isFavorite === true" class="fas fa-heart selected-favorite"></i>
-                  <i @click="toggleFavorite" v-if="isFavorite === false" class="fas fa-heart unselected-favorite"></i> -->
-                  <i @click="toggleFavorite" v-if="isFavorite === true" class="fas fa-heart selected-favorite"></i>
-                  <i @click="toggleFavorite" v-if="isFavorite === false" class="heart icon"></i>
+                  <i @click="toggleFavorite" v-if="isFavorite === true" class="fas fa-heart"></i>
+                  <i @click="toggleFavorite" v-if="isFavorite === false" class="far fa-heart"></i>
                 <!-- </b-button> -->
                 <span>찜 하기</span>
               </div>
@@ -521,51 +519,11 @@ $Phone: "screen and (max-width : 640px)";
       }
 
       & > .my-book {
-        // & i {
-        //   cursor: pointer;
-        // }
-        & .selected-favorite {
+        & i {
           color: #fb366e;
+          cursor: pointer;
         }
-        // & .unselected-favorite {
-        //   color: #000;
-        // }
-        & .heart.icon {
-          color: #000;
-          position: absolute;
-          margin-top: 6px;
-          margin-left: 5px;
-          width: 9px;
-          height: 9px;
-          border-left: solid 1px currentColor;
-          border-bottom: solid 1px currentColor;
-          -webkit-transform: rotate(-45deg);
-                  transform: rotate(-45deg);
-          ::before {
-            content: '';
-            position: absolute;
-            top: -5px;
-            left: -1px;
-            width: 8px;
-            height: 5px;
-            border-radius: 5px 5px 0 0;
-            border-top: solid 1px currentColor;
-            border-left: solid 1px currentColor;
-            border-right: solid 1px currentColor;
-          }
-          ::after {
-            content: '';
-            position: absolute;
-            top: 0px;
-            left: 8px;
-            width: 5px;
-            height: 8px;
-            border-radius: 0 5px 5px 0;
-            border-top: solid 1px currentColor;
-            border-right: solid 1px currentColor;
-            border-bottom: solid 1px currentColor;
-          }
-        }
+      
         & span {
           color: #000;
           font-size: 0.7rem;
